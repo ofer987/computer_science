993fcb45d265d0c1a4e258ee94ba64ffee310386
Dan Jakob Ofer <ofer987@gmail.com>
2016-09-25 11:12:40 -0400

    Improved solution for Password

--- custom/password/solution.rb
+++ custom/password/solution.rb
@@ -17,39 +17,11 @@ class Password
     @string = string.to_s.strip
   end
 
-  def each_valid
-    (start_index..end_index).each do |i|
-      (i..end_index).to_a.reverse.each do |j|
-        substring = string[i..j]
-
-        if (/\d/ =~ substring)
-          next
-        end
-
-        if (/[a-z]/ =~ substring && /[A-Z]/ =~ substring)
-          yield substring
-        end
-      end
-    end
-  end
-
   def longest_valid
-    longest = NilValidSubstring.new
-    each_valid do |valid|
-      longest = valid if valid.length > longest.length
-    end
-
-    longest
-  end
-
-  private
-
-  def start_index
-    0
-  end
-
-  def end_index
-    string.length - 1
+    string
+      .split(/\d/)
+      .sort { |a, b| b.length <=> a.length }
+      .find (-> { NilValidSubstring.new }) { |item| /[a-z]/ =~ item && /[A-Z]/ =~ item }
   end
 end
 
