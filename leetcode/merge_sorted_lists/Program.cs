bc9f7669f8b7b882f1046db9b71f5283054dc28b
Dan Jakob Ofer <ofer987@gmail.com>
2017-01-02 11:35:00 -0500

    New Problem: Merge k Sorted Lists
    Wrote a test case generate
    Invoke with:
    ./generate test_case_number max_number length_1 length_2 ... length_n

---
+++ leetcode/merge_sorted_lists/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace merge_sorted_lists
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
