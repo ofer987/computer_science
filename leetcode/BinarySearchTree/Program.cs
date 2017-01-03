d29a9b2edda5f78642b64992d54d280765364881
Dan Jakob Ofer <ofer987@gmail.com>
2017-01-03 17:27:50 -0500

    Developing Test Harness (Program.cs)

--- leetcode/BinarySearchTree/Program.cs
+++ leetcode/BinarySearchTree/Program.cs
@@ -11,6 +11,40 @@ namespace BinarySearchTree
     {
         public static void Main(string[] args)
         {
+            var numbers = ReadLines().SelectMany(number => number);
+            var tree = new Tree(numbers);
+
+            // Do something
+        }
+
+        private IEnumerable<IEnumerable<int?>> ReadLines()
+        {
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == null)
+                {
+                    yield break;
+                }
+
+                yield return ParseLine(line);
+            }
+        }
+
+        private IEnumerable<int?> ParseLine(string line)
+        {
+            foreach (var item in line.Split(' '))
+            {
+                int val;
+                if (int.TryParse(item, out val))
+                {
+                    yield return val;
+                }
+                else
+                {
+                    yield return null;
+                }
+            }
         }
     }
 }
